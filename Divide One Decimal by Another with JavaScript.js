ab286b9950accd9fdfb2824ca1a190b49a4ae5e3
Yasmine Ali <67972643+Yasmine-Aly@users.noreply.github.com>
2024-08-27 18:25:08 +0300

    Create Divide One Decimal by Another with JavaScript.js

---
+++ Divide One Decimal by Another with JavaScript.js
@@ -0,0 +1,4 @@
+//Change the 0.0 so that quotient will equal to 2.2.
+//const quotient = 0.0 / 2.0; // Change this line
+// Change the 0.0 so that the quotient will equal 2.2.
+const quotient = 4.4 / 2.0;  // 4.4 divided by 2.0 equals 2.2
