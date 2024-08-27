ec72450e261911d307928b1db9911185c9237ce1
Yasmine Ali <67972643+Yasmine-Aly@users.noreply.github.com>
2024-08-27 18:23:55 +0300

    Create Multiply Two Decimals with JavaScript.js

---
+++ Multiply Two Decimals with JavaScript.js
@@ -0,0 +1,4 @@
+//Change the 0.0 so that product will equal 5.0.
+//const product = 2.0 * 0.0;
+// Change the 0.0 so that the product will equal 5.0.
+const product = 2.0 * 2.5;  // 2.0 multiplied by 2.5 equals 5.0
