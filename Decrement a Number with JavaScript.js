7b442fd83405684ad94bc2280cdfd091232da198
Yasmine Ali <67972643+Yasmine-Aly@users.noreply.github.com>
2024-08-27 18:20:46 +0300

    Create Decrement a Number with JavaScript.js

---
+++ Decrement a Number with JavaScript.js
@@ -0,0 +1,11 @@
+//Change the code to use the -- operator on myVar.
+/*
+let myVar = 11;
+
+// Only change code below this line
+myVar = myVar - 1;
+*/
+let myVar = 11;
+
+// Only change code below this line
+myVar = --myVar;
