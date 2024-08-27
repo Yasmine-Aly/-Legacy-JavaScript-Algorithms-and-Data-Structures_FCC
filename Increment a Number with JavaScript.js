c46bd90b3a695d709eda69ec3ba1e99e5ddd3693
Yasmine Ali <67972643+Yasmine-Aly@users.noreply.github.com>
2024-08-27 18:19:42 +0300

    Create Increment a Number with JavaScript.js

---
+++ Increment a Number with JavaScript.js
@@ -0,0 +1,8 @@
+//let myVar = 87;
+// Only change code below this line
+//myVar = myVar + 1;
+//Change the code to use the ++ operator on myVar.
+let myVar = 87;
+
+// Only change code below this line
+myVar = ++myVar;
