4dc1c0b9831a4fa8474828ff6fdcc13c657ec5d3
Yasmine Ali <67972643+Yasmine-Aly@users.noreply.github.com>
2024-08-27 17:32:04 +0300

    Create Comment Your JavaScript Code.js

---
+++ Comment Your JavaScript Code.js
@@ -0,0 +1,7 @@
+Using // will tell JavaScript to ignore the remainder of the text on the current line. This is an in-line comment:
+
+// This is an in-line comment.
+You can make a multi-line comment beginning with /* and ending with */. This is a multi-line comment:
+
+/* This is a
+multi-line comment */
